7e9bf19b9237300ce71262704433a58396209d72
Nyleco-J <anuran.nyleco24@gmail.com>
2023-03-08 15:47:34 +0800

    starbucks

---
+++ script.js
@@ -0,0 +1,7 @@
+const kamote = document.querySelectorAll(".links__item h4");
+kamote.forEach((kamoteq) => {
+    kamoteq.addEventListener("click", () => {
+        kamoteq.nextElementSibling.classList.toggle("open");
+        kamoteq.querySelector("i").classList.toggle("open")
+    })
+})
